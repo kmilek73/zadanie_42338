19d6e03adc7a4d6c5607c0694e06c095a70afc2f
kmilek <kmilek73@gmail.com>
2022-12-08 14:13:30 +0100

    poproawione

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,12 @@
 
- const tablica = [1,2,3,4,5];
- console.log(tablica);
- console.log("Pierwsza cyfra z tablicy to : ", tablica[0]);
+const tablica = [1,2,3,4,5];
 
 
 
+function tablica_n(tab) {
+
+   console.log("Pierwsza cyfra z tablicy to : ", tab[0]);
+
+}
+
+tablica_n(tablica);
